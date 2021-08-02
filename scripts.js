7569b57b62fcdf869c6f95b75f3b5eb01aef36d2
frankdrebin69 <gaborvamos95@gmail.com>
2021-08-02 06:09:46 +0200

    improvements

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var color = "purple"
+var number = 10
+var word = "cool"
+
+if (color === "purple") {
+    $(".first-centered").css("background", color)
+}
+
+if (number > 100) {
+    $(".second-centered").text("wow, de nagy szám")
+} else {
+    $(".second-centered").text("ez csak egy közönséges szám")
+}
+
+if (word === "cool") {
+    $(".third-centered").text("A DOM ereje")
+} else {
+    $(".fourth-centered").text("A DOM ereje")
+}
